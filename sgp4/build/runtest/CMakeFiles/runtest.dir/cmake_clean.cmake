c874e1dd943ae2abc74c3a2f5f7f68e6fedace4c
Felipe Cortez <felipecortezfi@gmail.com>
2017-09-05 10:11:47 -0300

    SGP4 modificada adicionada ao repositório

---
+++ sgp4/build/runtest/CMakeFiles/runtest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runtest.dir/runtest.cpp.obj"
+  "runtest.pdb"
+  "runtest.exe"
+  "runtest.exe.manifest"
+  "libruntest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
